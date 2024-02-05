29b34a40342d4b10acb5882ab3e56260187e6e34
akkicool199 <30728430+akkicool199@users.noreply.github.com>
2024-02-05 11:09:56 +0530

    Review point close for NullSafety.kt

--- NullSafety.kt
+++ NullSafety.kt
@@ -1,9 +1,14 @@
 package assessment_1
 
 fun main(){
-    val name: String?=null
-    val len=if(name!=null) name.length else -1
 
-    println("name length is $len")
+    var name : String = "Akash"
+    name = null; //Not Allowed
+    println(name.length)
+
+    var name2 : String? = "Akash"
+    name2 = null
+    // This statement will not throw null pointer Exception
+    println(name2?.length)
 
 }
